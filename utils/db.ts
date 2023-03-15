b6d28e6f919631502710febf26dff9fdaba265da
DejanTeofilovic <dejanteofilovic2@gmail.com>
2023-03-15 16:19:28 +0900

    yeah

--- utils/db.ts
+++ utils/db.ts
@@ -3,14 +3,16 @@ import { DB_PORT } from "./constants";
 
 const { DB_NAME, DB_USERNAME, DB_PASSWORD, DB_HOST } = process.env;
 
+console.log({ DB_NAME, DB_USERNAME, DB_PASSWORD, DB_HOST });
+
 const db = ServerlessMysql({
   config: {
     database: DB_NAME,
     user: DB_USERNAME,
     password: DB_PASSWORD,
     host: DB_HOST,
-    port: DB_PORT,
-  },
+    port: DB_PORT
+  }
 });
 
 module.exports = db;
